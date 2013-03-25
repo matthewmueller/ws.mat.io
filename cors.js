983ee33e03f70992ef2ab62f1e644b59a37efe47
Matt Mueller <mattmuelle@gmail.com>
2013-03-24 18:09:33 -0700

    set credentials to false

--- cors.js
+++ cors.js
@@ -6,6 +6,7 @@ module.exports = function(req, res, next){
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods', 'GET');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  res.set('Access-Control-Allow-Credentials', false);
 
   // Respond OK if the method is OPTIONS
   if(req.method === 'OPTIONS') {
