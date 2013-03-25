f8eb4bc994105e56ab883b18394938eba341c875
Matt Mueller <mattmuelle@gmail.com>
2013-03-24 18:30:33 -0700

    added origin

--- cors.js
+++ cors.js
@@ -3,10 +3,10 @@
  */
 
 module.exports = function(req, res, next){
-  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Origin', req.headers.origin);
   res.set('Access-Control-Allow-Methods', 'GET');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  res.set('Access-Control-Allow-Credentials', false);
+  res.set('Access-Control-Allow-Credentials', true);
 
   // Respond OK if the method is OPTIONS
   if(req.method === 'OPTIONS') {
