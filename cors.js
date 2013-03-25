a96d58d47c46386ef4a7bf9e274d9fe3b2d7e9f9
Matt Mueller <mattmuelle@gmail.com>
2013-03-24 18:08:11 -0700

    cors

--- cors.js
+++ cors.js
@@ -6,7 +6,6 @@ module.exports = function(req, res, next){
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods', 'GET');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  res.set('Access-Control-Allow-Credentials', true);
 
   // Respond OK if the method is OPTIONS
   if(req.method === 'OPTIONS') {
