eddd6dfdc359c2eb2b1eb6263f5885c0541ba844
Matt Mueller <mattmuelle@gmail.com>
2013-04-05 03:42:54 -0700

    added naming and sending to individual clients

--- io.js
+++ io.js
@@ -5,7 +5,13 @@
 module.exports = IO;
 
 /**
- * Clients
+ * Pools, segmented by path
+ */
+
+var pools = {};
+
+/**
+ * Clients, segmented by "path|name"
  */
 
 var clients = {};
@@ -15,22 +21,39 @@ var clients = {};
  */
 
 function IO(socket) {
-  if(!(this instanceof IO)) return new IO(socket);
+  if (!(this instanceof IO)) return new IO(socket);
   this.socket = socket;
 
   var req = socket.transport.request,
-      path = this.path = req.query.pathname;
+      path = this.path = req.query.pathname,
+      name = this.name = req.query.name;
 
-  if(!clients[path]) clients[path] = [];
-  clients[path].push(socket);
+  // only permit sockets with pathnames
+  if(!path) return socket.close();
+
+  // if the socket has given a name, place in clients
+  if (name) {
+    var key = path + '|' + name;
+    if(clients[key]) {
+      return socket.send('{"event":"nak"}');
+    } else {
+      socket.send('{"event":"ack"}');
+      clients[key] = socket;
+    }
+  }
+
+  // add to pool
+  if (!pools[path]) pools[path] = [];
+  pools[path].push(socket);
 
   // handle incoming messages
   socket.on('message', this.message.bind(this));
 
   // cleanup
   socket.on('close', function() {
-    var i = clients[path].indexOf(socket);
-    if(~i) clients[path].splice(i, 1);
+    var i = pools[path].indexOf(socket);
+    if (~i) pools[path].splice(i, 1);
+    if(name) delete clients[path + '|' + name]
   });
 }
 
@@ -42,9 +65,16 @@ function IO(socket) {
  */
 
 IO.prototype.message = function(message) {
-  var path = this.path;
-  var socket = this.socket;
-  var group = clients[path];
+  var path = this.path,
+      socket = this.socket,
+      group = pools[path],
+      json = JSON.parse(message),
+      client;
+
+  if (json.to && (client = clients[path + '|' + json.to])) {
+    client.send(message);
+    return this;
+  }
 
   for (var i = 0, len = group.length; i < len; i++) {
     if(group[i] === socket) continue;
